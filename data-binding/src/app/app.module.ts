1df113687a4def4169ef7d36f7b72b660fc712a2
Alan Pinhel <alanpinhel@gmail.com>
2017-12-02 12:20:10 -0200

    Two-way Data Binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,8 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
