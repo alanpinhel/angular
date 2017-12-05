5ad97e3cd09f6b9b9de93e693d9d2b9278de999a
Alan Pinhel <alanpinhel@gmail.com>
2017-12-04 23:38:29 -0200

    Diretivas: ngFor

---
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngfor',
+  templateUrl: './diretiva-ngfor.component.html',
+  styleUrls: ['./diretiva-ngfor.component.scss']
+})
+export class DiretivaNgforComponent implements OnInit {
+  cursos = ['Angular 2', 'Java', 'Phonegap'];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
