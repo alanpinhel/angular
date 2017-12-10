29115018d4f4d82dcb50c415278d7a69cf73b133
Alan Pinhel <alan.pinhel@objective.com>
2017-12-10 18:15:09 -0200

    Rotas: Definindo e extraindo parâmetros de roteamento

---
+++ rotas/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent implements OnInit {
+
+  id: string;
+
+  constructor(
+    private _route: ActivatedRoute
+  ) {
+    this.id = this._route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+  }
+
+}
