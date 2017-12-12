6cd7c1570a0aa540f0eec635ece16c30199b5043
Alan Pinhel <alan.pinhel@objective.com.br>
2017-12-12 13:31:45 -0200

    Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Rx';
+
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.css']
 })
-export class AlunoDetalheComponent implements OnInit {
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  aluno: any;
+  inscricao: Subscription;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _alunosService: AlunosService
+  ) { }
 
   ngOnInit() {
+    this.inscricao = this._route.params
+      .subscribe((params: any) => {
+        let id = params['id'];
+        this.aluno = this._alunosService.getAluno(id);
+      });
+  }
+
+  editarContato() {
+    this._router.navigate(['/alunos', this.aluno.id, 'editar']);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos.routing.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
-  imports: [CommonModule, AlunosRoutingModule],
-  declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent]
+  imports: [CommonModule, AlunosRoutingModule, FormsModule],
+  declarations: [
+    AlunosComponent,
+    AlunoFormComponent,
+    AlunoDetalheComponent
+  ],
+  providers: [AlunosService]
 })
 export class AlunosModule { }
