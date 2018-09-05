b25c5637d6bfdfa5a3a7a418bb6def8cfa0af5f2
Alan Pinhel <alan.pinhel@matera.com>
2018-09-05 11:39:15 -0300

    Aula 06 - Melhorando a experiência do usuário

---
+++ alura/alurapic/src/app/photos/photo-list/load-button/load-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ap-load-button',
+  templateUrl: './load-button.component.html',
+  styleUrls: ['./load-button.component.css']
+})
+export class LoadButtonComponent {
+  @Input() hasMore;
+}
---
+++ alura/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
+import { PhotoService } from './../photo/photo.service';
+
+@Component({
+  selector: 'ap-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit, OnDestroy {
+  photos: Photo[] = [];
+  filter = '';
+  debounce = new Subject<string>();
+  userName = '';
+  currentPage = 1;
+  hasMore = true;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
+
+  ngOnInit() {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listUserPhotosPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
+  }
+}
